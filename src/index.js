425f7cea1a2273f52dcf5d72f486139f282ae7a5
nronzel <86695181+nronzel@users.noreply.github.com>
2023-01-03 17:32:27 -0500

    prettified

--- src/index.js
+++ src/index.js
@@ -24,12 +24,3 @@ list.removeProject("Project2")
 // create task
 proj1.addTask(new Task('test task'))
 proj1.addTask(new Task('hope this works'))
-
-console.log(list.projects)
-console.log(proj1.tasks)
-
-// test remove task
-// proj1.removeTask('test task')
-// console.log(proj1.tasks)
-
-console.log(proj1.getTask('test task'))
