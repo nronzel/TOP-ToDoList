a4b84a26185ca4fa1f9e718dc50464bdd91f404c
nronzel <86695181+nronzel@users.noreply.github.com>
2023-01-07 20:58:54 -0500

    added functionality to add/remove tasks and draw them on screen

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import { Task } from "../src/task.js";
-import { openModal, closeModal, getModalTitle } from "../src/modal.js";
+import { openProjModal, closeModal, getProjModalTitle } from "../src/modal.js";
 import { Project, ProjectList } from "./crud.js"
 
-// ProjectList is a an object that contains a list to house all projects, 
-//   and methods to add/remove/get.
+// ProjectList is a an object that contains a list to house all projects
+//   plus methods to add/remove/get.
+
 // Project is a class that creates new projects 
-//   with their own list that contains tasks for that project
+//   with their own list that contains tasks for each project
 //   as well as methods to add/remove/get tasks
 
 //
@@ -35,7 +36,7 @@ const makeCardActive = (li) => {
   }
 };
 
-const makeProjectCards = (project) => {
+const drawProjectCards = () => {
   clearProjectListDisplay();
   ProjectList.list.forEach((project) => {
     let li = document.createElement("li");
@@ -55,13 +56,13 @@ const makeProjectCards = (project) => {
 };
 
 const getNewProject = () => {
-  let title = getModalTitle();
+  let title = getProjModalTitle();
   if (title === undefined || title.length < 1) {
     return;
   } else {
     let project = new Project(title);
     ProjectList.add(project);
-    makeProjectCards(project);
+    drawProjectCards()
     domCache.modalInput.value = "";
     closeModal();
   }
@@ -103,10 +104,80 @@ const removeProject = (e) => {
   ProjectList.remove(projTitle);
 };
 
-domCache.newProjectBtn.onclick = openModal;
+domCache.newProjectBtn.onclick = openProjModal;
 domCache.modalSubmit.onclick = getNewProject;
 domCache.overlay.onclick = closeModal;
 domCache.modalCloseBtn.onclick = closeModal;
+
+
+
+
+// Tasks
+const taskCache = {
+  newTaskBtn: document.querySelector(".new-task-btn"),
+  taskListContainer: document.querySelector(".todo-container"),
+  taskList: document.querySelector('.task-list')
+}
+
+const drawTaskCards = (project) => {
+  clearTaskDisplay() // TODO define this
+  console.log(project)
+  project.list.forEach((task) => {
+    let taskContainer = document.createElement("div")
+    let taskIcon = document.createElement("img")
+    let taskSubject = document.createElement("p")
+    let removeBtn = document.createElement("img")
+
+    taskContainer.classList.add("task")
+    taskIcon.src = "../src/circle-in-circle.svg"
+    taskIcon.classList.add("task-icon")
+    taskIcon.classList.add("complete-btn")
+    taskIcon.alt = "task-not-complete"
+
+    taskSubject.classList.add("task-subject")
+    taskSubject.textContent = task.title
+
+    removeBtn.classList.add("task-icon")
+    removeBtn.src = "../src/trash.svg"
+    removeBtn.alt = "trash-can"
+
+
+    taskIcon.onclick = completeTask
+    removeBtn.onclick = removeTaskCard
+
+    taskContainer.append(taskIcon)
+    taskContainer.append(taskSubject)
+    taskContainer.append(removeBtn)
+
+    taskCache.taskListContainer.append(taskContainer)
+  })
+}
+
+const removeTaskCard = (e) => {
+  e.target.parentElement.outerHTML = ""
+}
+
+const completeTask = (e) => {
+  let completeBtn = e.target
+  if (completeBtn.parentElement.classList.contains("complete")) {
+    completeBtn.src = "../src/circle-in-circle.svg"
+    completeBtn.classList.remove("complete")
+    completeBtn.parentElement.classList.remove("complete")
+    completeBtn.nextSibling.style.color = "var(--white)"
+  } else {
+    completeBtn.src = "../src/circle-check.svg"
+    completeBtn.parentElement.classList.add("complete")
+    completeBtn.nextSibling.style.color = "#656565"
+  }
+
+}
+
+const clearTaskDisplay = () => {
+  console.log('cleared')
+}
+
+// taskCache.newTaskBtn.onclick =
+
 
 // test projects
 const proj1 = new Project("Project1");
@@ -115,10 +186,12 @@ const proj3 = new Project("Project3");
 ProjectList.add(proj1);
 ProjectList.add(proj2);
 ProjectList.add(proj3);
-makeProjectCards(proj1);
-makeProjectCards(proj2);
-makeProjectCards(proj3);
+drawProjectCards(proj1);
+drawProjectCards(proj2);
+drawProjectCards(proj3);
 
 // create task
 proj1.add(new Task("test task"));
 proj1.add(new Task("hope this works"));
+
+drawTaskCards(proj1)
