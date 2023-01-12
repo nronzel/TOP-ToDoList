20fd235fa80310595f7f3551da3322637f10bebf
nronzel <86695181+nronzel@users.noreply.github.com>
2023-01-12 12:06:26 -0500

    create List class

---
+++ src/modules/list.js
@@ -0,0 +1,45 @@
+import Project from './project.js'
+import Task from './task.js'
+
+export default class List {
+  constructor() {
+    this.projects = [];
+    this.projects.push(new Project('Today'));
+    this.projects.push(new Project('Tomorrow'));
+  }
+
+  setProjects(projects) {
+    this.projects = projects;
+  }
+
+  getProjects() {
+    return this.projects;
+  }
+
+  getProject(title) {
+    return this.projects.find((project) => project.getTitle() === title);
+  }
+
+  contains(title) {
+    return this.projects.some((project) => project.getTitle() === title)
+  }
+
+  addProject(newProject) {
+    if (this.projects.find((project) => project.name === newProject.name))
+      return
+    this.projects.push(newProject);
+    }
+
+  removeProject(title) {
+    const toDelete = this.projects.find((project) => project.getName() === title);
+    this.projects.splice(this.projects.indexOf(toDelete), 1);
+  }
+
+  // updateTodayProject() {
+
+  // }
+
+  // updateTomorrowProject() {
+
+  // }
+}
