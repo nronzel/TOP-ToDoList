2e95562816d3f8b70274fd9aaaad429f71f1f261
nronzel <86695181+nronzel@users.noreply.github.com>
2023-01-12 11:58:07 -0500

    added Task class

---
+++ src/modules/task.js
@@ -0,0 +1,29 @@
+export default class Task {
+  constructor(title, dueDate = "No Due Date") {
+    this.title = title;
+    this.dueDate = dueDate;
+  }
+
+  setTitle(title) {
+    this.title = title;
+  }
+
+  getTitle() {
+    return this.title;
+  }
+
+  setDate(dueDate) {
+    this.dueDate = dueDate;
+  }
+
+  getDate() {
+    return this.dueDate;
+  }
+
+  getDateFormatted() {
+    const day = this.dueDate.split('/')[0]
+    const month = this.dueDate.split('/')[1]
+    const year = this.dueDate.split('/')[2]
+    return `${month}/${day}/${year}`
+  }
+}
